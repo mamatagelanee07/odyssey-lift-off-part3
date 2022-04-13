9d2b3c0d87d650959301c40397cde9094f79ebd8
mamatagelanee07 <gelaneem@email.com>
2022-04-13 07:35:54 +0100

    Added get track by id in graphql

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,9 +1,10 @@
-const { gql } = require('apollo-server');
+const { gql } = require("apollo-server");
 
 const typeDefs = gql`
   type Query {
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
+    track(id : ID!) : Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -19,6 +20,21 @@ const typeDefs = gql`
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+    "The track's complete description, can be in Markdown format"
+    description: String
+    "The number of times the track has been viewed"
+    numberOfViews: Int
+    "The track's complete array of Modules"
+    modules : [Module!]
+  }
+
+  "A Module is a single unit if teachng. Multiple modules compose a Track"
+  type Module {
+    id: ID!
+    "The module's title"
+    title: String!
+    "The module's length in minutes"
+    length: Int
   }
 
   "Author of a complete Track or a Module"
